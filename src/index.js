d6ae5a50827fb9ae420e26b60dd1fb1e2cd416c9
Dale R. Hille <dalehill@us.ibm.com>
2020-05-14 09:58:44 -0400

    WIP

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 const log = require('../lib/bunyan-api').createLogger('cluster-subscription');
 
 const { createRemoteResources, getRemoteResources, deleteRemoteResources } = require('../lib/remoteResource');
-const { subscriptionClient, getSubscriptions} = require('../lib/subscriptions');
+const { subscriptionClient, getSubscriptions } = require('../lib/subscriptions');
 
-const ORG_ID = process.env.RAZEE_ORG_ID;
 const ORG_KEY = process.env.RAZEE_ORG_KEY;
 const RAZEE_API = process.env.RAZEE_API;
 const RAZEE_TAGS = process.env.RAZEE_TAGS;
 
-if (!ORG_ID) {
-  throw 'Please specify process.env.RAZEE_ORG_ID';
-}
 if (!ORG_KEY) {
   throw 'Please specify process.env.RAZEE_ORG_KEY';
 }
@@ -35,10 +31,10 @@ subscriptionClient.subscribe( (event) => {
 
 const init = async() => {
   // rr's on this cluster with the 'deploy.razee.io/clustersubscription' annotation
-  const clusterResources = await getRemoteResources();
-  log.debug('cluster remote resources:', clusterResources);
+  // const clusterResources = await getRemoteResources();
+  // log.debug('cluster remote resources:', clusterResources);
 
-  // list of razee subscriptions for this org id
+  // // list of razee subscriptions for this org id
   const res = await getSubscriptions(RAZEE_TAGS).catch( () => false );
   const subscriptions = (res && res.data && res.data.subscriptionsByTag) ? res.data.subscriptionsByTag : false;
   log.debug('razee subscriptions:', subscriptions);
@@ -46,34 +42,34 @@ const init = async() => {
   // 
   // Create remote resources
   // 
-  if(subscriptions) {
-    await createRemoteResources(subscriptions);
-    log.info('finished creating remote resources');
-  } else {
-    log.debug('no remote resources need to be created');
-  }
+  // if(subscriptions) {
+  //   await createRemoteResources(subscriptions);
+  //   log.info('finished creating remote resources');
+  // } else {
+  //   log.debug('no remote resources need to be created');
+  // }
 
   // 
   // Delete remote resources
   // 
-  if(subscriptions && clusterResources && clusterResources.length > 0) {
-    log.info('looking for remote resources to delete...');
+  // if(subscriptions && clusterResources && clusterResources.length > 0) {
+  //   log.info('looking for remote resources to delete...');
     
-    const subscriptionUuids = subscriptions.map( (sub) => sub.subscription_uuid ); // uuids from razee
-    const invalidResources = clusterResources.filter( (rr) => {
-      // the annotation looks like: deploy.razee.io/clustersubscription: 89cd2717-c7f5-43d6-91a7-fd1ec44e1abb
-      return subscriptionUuids.includes(rr.metadata.annotations['deploy.razee.io/clustersubscription']) ? false : true;
-    });
+  //   const subscriptionUuids = subscriptions.map( (sub) => sub.subscription_uuid ); // uuids from razee
+  //   const invalidResources = clusterResources.filter( (rr) => {
+  //     // the annotation looks like: deploy.razee.io/clustersubscription: 89cd2717-c7f5-43d6-91a7-fd1ec44e1abb
+  //     return subscriptionUuids.includes(rr.metadata.annotations['deploy.razee.io/clustersubscription']) ? false : true;
+  //   });
  
-    const invalidSelfLinks = invalidResources.map( (resource) => resource.metadata.selfLink );
-    if(invalidSelfLinks.length > 0) {
-      await deleteRemoteResources(invalidSelfLinks);
-    } else {
-      log.debug('existing remote resources are valid. nothing to delete');
-    }
-  } else {
-    log.debug('no remote resources need to be deleted');
-  }
+  //   const invalidSelfLinks = invalidResources.map( (resource) => resource.metadata.selfLink );
+  //   if(invalidSelfLinks.length > 0) {
+  //     await deleteRemoteResources(invalidSelfLinks);
+  //   } else {
+  //     log.debug('existing remote resources are valid. nothing to delete');
+  //   }
+  // } else {
+  //   log.debug('no remote resources need to be deleted');
+  // }
 };
 
 init();
